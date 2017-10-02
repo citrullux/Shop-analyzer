cd2f2da8a2db2d6fc83d72c4e530b07f043fd337
Дмитрий Абдрахманов <565963@gmail.com>
2017-10-02 23:30:16 +0300

    Add Class

---
+++ Shop-analyzer/ClassLibrary1/Indicators.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indicators
+{
+    public class Indicators
+    {
+        [System.ComponentModel.DisplayName("Годовая экономия (Прибыль)")]
+        public double YearSave { get; set; }
+        [System.ComponentModel.DisplayName("Годовой экономический эффект")]
+        public double YearEffect { get; set; }
+    }
+}
